da3eeaaa58471a17f5610c0c8d4daab2a054747c
Luli <lulimanucians198.com>
2024-05-07 18:42:29 +0700

    lan1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function MyButton(){
+  return(
+    <button>TBD</button>
+  );
+}
+export default function MyApp(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> Thái Bình Độ</h1>
+      <MyButton />
+    </>
   );
 }
 
-export default App;
+
